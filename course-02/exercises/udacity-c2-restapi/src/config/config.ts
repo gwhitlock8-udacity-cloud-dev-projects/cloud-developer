25dcd5f06e3ba03a9ceec9127d951aa964cdd7b4
Gavin Whitlock <gavinswhitlock@gmail.com>
2020-06-24 17:49:53 -0500

    pushing for support

--- course-02/exercises/udacity-c2-restapi/src/config/config.ts
+++ course-02/exercises/udacity-c2-restapi/src/config/config.ts
@@ -1,19 +1,19 @@
 export const config = {
-  "dev": {
-    "username": "",
-    "password": "",
-    "database": "",
-    "host": "",
-    "dialect": "postgres",
-    "aws_region": "us-east-2",
-    "aws_profile": "default",
-    "aws_media_bucket": "udagram-ruttner-dev"
+  dev: {
+    username: "udagramgwdev",
+    password: "udagramgwdev",
+    database: "udagramgwdev",
+    host: "udagramwhitlockdev.cwpbc0leojra.us-east-2.rds.amazonaws.com",
+    dialect: "postgres",
+    aws_region: "us-east-2",
+    aws_profile: "default",
+    aws_media_bucket: "udcity-project-dev",
   },
-  "prod": {
-    "username": "",
-    "password": "",
-    "database": "udagram_prod",
-    "host": "",
-    "dialect": "postgres"
-  }
-}
+  prod: {
+    username: "",
+    password: "",
+    database: "udagram_prod",
+    host: "",
+    dialect: "postgres",
+  },
+};
