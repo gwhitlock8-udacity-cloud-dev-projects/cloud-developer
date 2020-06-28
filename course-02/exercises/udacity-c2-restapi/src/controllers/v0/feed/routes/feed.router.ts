042fcf955a2094ac009375b943935d9b41f8af67
Gavin Whitlock <gavinswhitlock@gmail.com>
2020-06-28 14:29:08 -0500

    added the requireAuth fx to feed router

--- course-02/exercises/udacity-c2-restapi/src/controllers/v0/feed/routes/feed.router.ts
+++ course-02/exercises/udacity-c2-restapi/src/controllers/v0/feed/routes/feed.router.ts
@@ -6,7 +6,7 @@ import * as AWS from "../../../../aws";
 const router: Router = Router();
 
 // Get all feed items
-router.get("/", async (req: Request, res: Response) => {
+router.get("/", requireAuth, async (req: Request, res: Response) => {
   const items = await FeedItem.findAndCountAll({ order: [["id", "DESC"]] });
   items.rows.map((item) => {
     if (item.url) {
@@ -18,7 +18,7 @@ router.get("/", async (req: Request, res: Response) => {
 
 //@TODO
 //Add an endpoint to GET a specific resource by Primary Key
-router.get("/:id", async (req: Request, res: Response) => {
+router.get("/:id", requireAuth, async (req: Request, res: Response) => {
   let { id } = req.params;
   try {
     const item = await FeedItem.findByPk(id);
